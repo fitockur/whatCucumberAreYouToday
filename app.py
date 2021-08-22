529d470b557a724055bb0a10397c837fb9fb077c
Никита Козырев <kozyrevnm@gmail.com>
2021-08-23 01:03:16 +0300

    add /cucumbers/

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify
+from cucumber_scrapper import cucumber_scrapper
+import yaml
+
+
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -22,23 +26,15 @@ def respond():
         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
     # Return the response in json format
-    return jsonify(response)
+    return jsonify(response) 
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+@app.route('/cucumbers/')
+def get_cucumbers():
+    with open('cucumbers.yml', 'r', encoding='utf-8') as f:
+        cucumbers = yaml.safe_load(f)
+
+    # Return the response in json format
+    return jsonify(cucumbers)
 
 # A welcome message to test our server
 @app.route('/')
@@ -46,6 +42,7 @@ def index():
     return "Welcome to our server !!"
 
 if __name__ == '__main__':
+    with open('cucumbers.yml', 'w', encoding='utf-8') as f:
+        yaml.safe_dump(cucumber_scrapper(), f)
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
-    
--- cucumber_scrapper.py
+++ cucumber_scrapper.py
@@ -1,19 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import yaml
 
-url = 'https://stroy-podskazka.ru/ogurec/sorta/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
 
-alphabet_groups = soup.find_all('div', class_='alphabet__group')
+def cucumber_scrapper(url='https://stroy-podskazka.ru/ogurec/sorta/'):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-cucumbers = {}
-for alphabet_group in alphabet_groups:
-    alphabet = alphabet_group.find('div', class_='hl').text
-    cucumbers[alphabet] = {}
-    for cucumber in alphabet_group.find_all('a', class_='perelink_gallery_item_sorta'):
-        cucumbers[alphabet][cucumber.img['alt']] = cucumber.img['src']
+    alphabet_groups = soup.find_all('div', class_='alphabet__group')
 
-with open('cucumbers.yml', 'w', encoding='utf-8') as f:
-    yaml.dump(cucumbers, f)
+    cucumbers = {}
+    for alphabet_group in alphabet_groups:
+        alphabet = alphabet_group.find('div', class_='hl').text
+        cucumbers[alphabet] = {}
+        for cucumber in alphabet_group.find_all('a', class_='perelink_gallery_item_sorta'):
+            cucumbers[alphabet][cucumber.img['alt']] = cucumber.img['src']
+    return cucumbers
