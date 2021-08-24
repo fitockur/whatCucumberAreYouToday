6e46039a0b5aa00579a19476e7bce9b50151fdad
Никита Козырев <kozyrevnm@gmail.com>
2021-08-24 16:12:32 +0300

    add form to get cucumber

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
 from app import app, cucumbers
+import numpy as np
 from app.forms import CucumberForm
 from flask import render_template
 
 
-@app.route('/cucumbers-1337/')
-def get_cucumbers():
-    # Return the cucumbers
-    return render_template('cucumbers.html', cucumbers=cucumbers)
-
-# A welcome message to test our server
-@app.route('/')
-@app.route('/index/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index/', methods=['GET', 'POST'])
 def index():
     form = CucumberForm()
-    return render_template('index.html', name='Nikita', form=form)
+    cucumber_flag = form.validate_on_submit()
+    if cucumber_flag:
+        idx = np.random.randint(low=0, high=len(cucumbers))
+        cucumber_text, cucumber_name, \
+            cucumber_url, cucumber_desc = cucumbers[idx]
+        kwargs = dict(
+            form=form, 
+            cucumber_flag=cucumber_flag, 
+            cucumber_name=cucumber_name, 
+            cucumber_url=cucumber_url,
+            cucumber_desc=cucumber_desc,
+            cucumber_text=cucumber_text
+        )
+        return render_template('index.html', **kwargs)
+    return render_template('index.html', form=form)
